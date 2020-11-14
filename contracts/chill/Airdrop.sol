0e112eb7f1ff4234bb67b11663043cb3e03971d6
formefreelance <formefreelance@gmail.com>
2020-11-14 09:36:05 +0530

    Refactor Airdrop code and Add Some logics

--- contracts/chill/Airdrop.sol
+++ contracts/chill/Airdrop.sol
@@ -6,40 +6,33 @@ import "../helper/SafeMath.sol";
 
 contract AirDrop {
     using SafeMath for uint256;
-    IERC20 chillToken;
+    
+    IERC20 public chillToken;
     IChillFinance public iChillFiinance;
-    uint256 nirwanaReward;
-    address public lpTokenAddress;
-    address[] public users;
-    uint256 timeStamp;
-    uint256 claimTimeStamp;
-    uint256 timeSchedule;
-    uint256 claimSchedule;
+    
     address public owner;
-    uint256 public percentage;
+    
+    uint256 public nirwanaReward;
+    uint256 public timeSchedule;
+    uint256 public claimSchedule;
+    uint256 public timeStamp;
+    uint256 public claimTimeStamp;
+    uint256 public rewardAmount;
     uint256 public scheduleCount = 0;
+    uint256 public NIRVANA_MULTIPLIER = 50;
+    
     mapping (uint256 => mapping(address => bool)) public isNewRewardGiven;
     
-    constructor(address _lpToken, address _owner) public {
+    constructor(address _owner) public {
         iChillFiinance = IChillFinance(0xa15E697806711003E635bEe08CA049130C4917fd);
         chillToken = IERC20(0xC059Ab991c99D2c08A511F8e04EE5EA85a2e97bf);
-        nirwanaReward = 1;
-        timeSchedule = 28800;
-        claimSchedule = 55800;
-        lpTokenAddress = _lpToken;
         owner = _owner;
-        timeStamp = block.timestamp.add(timeSchedule);
-        claimTimeStamp = block.timestamp.add(claimSchedule);
-    }
-
-    modifier isSchedule {
-        require(block.timestamp > timeStamp);
-        _;
-    }
-    
-    modifier isClaimSchedule {
-        require(block.timestamp < claimTimeStamp);
-        _;
+        nirwanaReward = 10; // Nirvana Reward Percentage
+        timeSchedule = 28800; // 8 hours
+        claimSchedule = 27000; // 15 hours 30 mins
+        uint256 currentTimeStamp = getCurrentTimeStamp();
+        timeStamp = currentTimeStamp.add(timeSchedule);
+        claimTimeStamp = currentTimeStamp.add(timeSchedule).add(claimSchedule);
     }
     
     modifier isOwner {
@@ -47,47 +40,49 @@ contract AirDrop {
         _;
     }
     
-    function getAllUsers(uint256 _pid) public view returns(address[] memory, uint256) {
-        uint256 userLength = iChillFiinance.userPoollength(_pid);
-        address[] memory nirvanaAddresses = new address[](userLength);
-        address poolUser;
-        uint256 count = 0;
-        for(uint i=0; i < userLength; i++) {
-            poolUser = iChillFiinance.poolUsers(_pid, i);
-            (,,uint256 startedBlock) = getUsersInfo(_pid, poolUser);
-            uint256 nirvanMultiplier = iChillFiinance.getNirvanaStatus(startedBlock);
-            if(nirvanMultiplier == 50) {
-                nirvanaAddresses[count] = poolUser;
-                count = count.add(1);
-            }
-        }
-        return (nirvanaAddresses, count);
-    }
-    
-    function setNewScheduler() public {
-        if (block.timestamp > timeStamp) {
-            timeStamp = block.timestamp.add(timeSchedule);
+    function setNewScheduler() internal {
+        if (getCurrentTimeStamp() > timeStamp) {
+            uint256 currentTimeStamp = getCurrentTimeStamp();
+            timeStamp = currentTimeStamp.add(timeSchedule); // increase by 8 hours
             uint256 chillBalance = chillToken.balanceOf(address(this));
             uint256 chillReward = chillBalance.mul(nirwanaReward).div(100);
-            percentage = chillReward;
+            rewardAmount = chillReward;
             scheduleCount = scheduleCount.add(1);
+            claimTimeStamp = currentTimeStamp.add(claimSchedule); // 7 hours 30 mins
         }
     }
 
     function claimNirvanaReward(uint256 _pid) public returns(bool) {
         (,,uint256 startedBlock) = getUsersInfo(_pid, msg.sender);
         uint256 nirvanMultiplier = iChillFiinance.getNirvanaStatus(startedBlock);
-        require(nirvanMultiplier == 50, "You are not Niravana user.");
+        require(nirvanMultiplier == NIRVANA_MULTIPLIER, "You are not Niravana user.");
         setNewScheduler();
         require(scheduleCount > 0, "Claim window is not open");
-        require(!isNewRewardGiven[scheduleCount][msg.sender], "Already Reward is Claimed, Wait for Next Snapshot");
+        require(!isNewRewardGiven[scheduleCount][msg.sender] && getCurrentTimeStamp() < claimTimeStamp, "Already Reward is Claimed, Wait for Next Snapshot");
         isNewRewardGiven[scheduleCount][msg.sender] = true;
         (,,,,uint256 poolBalance,,) = getPoolInfo(_pid);
         (uint256 userBalance,,) = getUsersInfo(_pid, msg.sender);
         uint256 transferBalancePercent = userBalance.div(poolBalance).mul(100);
-        uint256 transferBalance = transferBalancePercent.mul(percentage).div(100);
+        uint256 transferBalance = transferBalancePercent.mul(rewardAmount).div(100);
         chillToken.transfer(msg.sender, transferBalance);
         return true;
+    }
+    
+    function getAllUsers(uint256 _pid) public view returns(address[] memory, uint256) {
+        uint256 userLength = iChillFiinance.userPoollength(_pid);
+        address[] memory nirvanaAddresses = new address[](userLength);
+        address poolUser;
+        uint256 count = 0;
+        for (uint i = 0; i < userLength; i++) {
+            poolUser = iChillFiinance.poolUsers(_pid, i);
+            (,,uint256 startedBlock) = getUsersInfo(_pid, poolUser);
+            uint256 nirvanMultiplier = iChillFiinance.getNirvanaStatus(startedBlock);
+            if (nirvanMultiplier == NIRVANA_MULTIPLIER) {
+                nirvanaAddresses[count] = poolUser;
+                count = count.add(1);
+            }
+        }
+        return (nirvanaAddresses, count);
     }
     
     function setNirvanaReward(uint256 _rewards) public isOwner {
@@ -96,6 +91,14 @@ contract AirDrop {
 
     function setTimeSchedule(uint256 _timeSchedule) public isOwner {
         timeSchedule = _timeSchedule;
+    }
+
+    function setClaimTimeSchedule(uint256 _claimSchedule) public isOwner {
+        claimSchedule = _claimSchedule;
+    }
+    
+    function setNirVanaMultiplier(uint256 _nirvanaMultiplier) public isOwner {
+        NIRVANA_MULTIPLIER = _nirvanaMultiplier;
     }
 
     function getUsersInfo(uint256 _pid, address _user) public view returns (uint256, uint256, uint256) {
@@ -108,5 +111,9 @@ contract AirDrop {
     
     function setChillFinance(address _chillFinance) public isOwner {
         iChillFiinance = IChillFinance(_chillFinance);
+    }
+    
+    function getCurrentTimeStamp() public view returns(uint256) {
+        return block.timestamp;
     }
 }
