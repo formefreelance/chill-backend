fe05055a50a496cb5052f06e9b9a1108f5595eff
formefreelance <formefreelance@gmail.com>
2020-12-18 03:34:11 +0530

    new Instastake contracts for Fast Staking

--- contracts/chill/AirDropImplementation.sol
+++ contracts/chill/AirDropImplementation.sol
@@ -81,7 +81,7 @@ contract AirDropImplementation is IAirDrop  {
         uint256 transferBalance = transferBalancePercent.mul(rewardAmount).div(1e18);
         require(chillToken.balanceOf(address(this)) >= transferBalance.div(1e18), "Not Enough Balance in Nirvana Pool.");
         chillToken.transfer(msg.sender, transferBalance.div(1e18));
-        return true;
+        return true;    
     }
     
     function getAllUsers(uint256 _pid) external override view returns(address[] memory, uint256) {
