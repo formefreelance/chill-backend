d157ba7d1dc0df7e7d48ac2ea4abd749f0aa3edc
formefreelance <formefreelance@gmail.com>
2020-12-20 02:10:43 +0530

    Added upgraded Instastake fast staking contract

--- contracts/chill/InstaStake.sol
+++ contracts/chill/InstaStake.sol
@@ -1,20 +1,15 @@
 pragma solidity ^0.6.0;
 
-import "../uniswap/UniswapV2Router02.sol";
 import "../uniswap/UniswapV2Library.sol";
-import "../interfaces/IERC20.sol";
 import "../helper/SafeMath.sol";
 import "../helper/Ownable.sol";
 import "../interfaces/IUniswapV2Router02.sol";
 import "../interfaces/IUniswapV2Factory.sol";
 import "../interfaces/IUniswapV2Pair.sol";
-import "../interfaces/IWETH.sol";
 import "../helper/WETH9.sol";
+import "../helper/SafeERC20.sol";
 
-// 0x224AB8bC86788EE3EE91B5512D0Afd349a2b59aA
-// "0xC059Ab991c99D2c08A511F8e04EE5EA85a2e97bf",["0xd0A1E359811322d97991E03f863a0C30C2cF029C","0xC059Ab991c99D2c08A511F8e04EE5EA85a2e97bf"]
-
-contract InstaStake is Ownable {
+contract InstaStakeContract is Ownable {
 
     IUniswapV2Router02 public iUniswapV2Router02;
     IUniswapV2Factory public iUniswapV2factory;
@@ -22,14 +17,14 @@ contract InstaStake is Ownable {
     WETH9 public wethContract;
     address payable weth = 0xd0A1E359811322d97991E03f863a0C30C2cF029C;
     using SafeMath for uint256;
-    bool public isWethEnable = true;
     bool public isFastStaking = true;
+    using SafeERC20 for IERC20;
 
-    constructor(WETH9 _weth) public payable {
+
+    constructor(WETH9 _weth) public {
         wethContract = _weth;
         iUniswapV2Router02 = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
         iUniswapV2factory = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
-        wethContract.deposit{value : msg.value}();
     }
     
     receive() external payable {
@@ -41,10 +36,7 @@ contract InstaStake is Ownable {
     }
 
     function deposit(address _token, address[] memory _path) public payable isFastStakingEnable {
-        uint256 wethBal = IERC20(weth).balanceOf(address(this));
-        uint256 chillBal = IERC20(_token).balanceOf(address(this));
         wethContract.deposit{value : msg.value}();
-        IERC20(weth).approve(address(iUniswapV2Router02), 100000e18);
         uint256[] memory amounts = iUniswapV2Router02.swapExactTokensForTokens(
             msg.value.div(2),
             0,
@@ -61,29 +53,15 @@ contract InstaStake is Ownable {
             amountOut = UniswapV2Library.getAmountOut(amounts[1], reserve1, reserve0);
         }
         
-        IERC20(weth).approve(address(iUniswapV2Router02), amountOut);
-        IERC20(_token).approve(address(iUniswapV2Router02), amounts[1]);
         iUniswapV2Router02.addLiquidity(weth, _token, amountOut, amounts[1], 0, 0, msg.sender, block.timestamp.add(3600));
-        
-        uint256 restWethBal = IERC20(weth).balanceOf(address(this));
-        uint256 restChillBal = IERC20(weth).balanceOf(address(this));
-
-        if(restWethBal > wethBal) {
-            if(isWethEnable) {
-                IERC20(weth).transfer(msg.sender, restWethBal.sub(wethBal));
-            } else {
-                IWETH(weth).withdraw(restWethBal.sub(wethBal));
-                msg.sender.transfer(restWethBal.sub(wethBal));
-            }
-        }
-        
-        if(restChillBal > chillBal) {
-            IERC20(_token).transfer(msg.sender, restChillBal.sub(chillBal));
-        }
     }
     
-    function setWethEnable(bool _isWethEnable) public onlyOwner {
-        isWethEnable = _isWethEnable;
+    function approveUnlimited(address _token) public onlyOwner {
+        IERC20(_token).safeApprove(address(iUniswapV2Router02), uint(-1));
+    }
+    
+    function approveLimited(address _token, uint256 _amount) public onlyOwner {
+        IERC20(_token).safeApprove(address(iUniswapV2Router02), _amount);
     }
     
     function setFastStakingEnable(bool _isFastStaking) public onlyOwner {
